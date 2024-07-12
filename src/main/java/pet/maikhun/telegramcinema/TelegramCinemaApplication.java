a6508dab98920530da739cb4fc7042d1ab93990d
Anton Maryushkin <a.maryushkin@bk.ru>
2024-07-12 10:59:57 +0300

    Перенос каркаса проекта в основную

---
+++ src/main/java/pet/maikhun/telegramcinema/TelegramCinemaApplication.java
@@ -0,0 +1,13 @@
+package pet.maikhun.telegramcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pet/maikhun/telegramcinema/TelegramCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.maikhun.telegramcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
