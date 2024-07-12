3fa8aac16bcdd8b553b789dc4e7244d800c9630f
Anton Maryushkin <a.maryushkin@bk.ru>
2024-07-12 16:56:24 +0300

    Добавление интеграции с ботом telegram

---
+++ src/main/java/pet/maikhun/telegramcinema/config/BotInitializer.java
@@ -0,0 +1,32 @@
+package pet.maikhun.telegramcinema.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+public class BotInitializer {
+
+    private final TelegramCinemaBot telegramCinemaBot;
+
+    public BotInitializer(TelegramCinemaBot telegramCinemaBot) {
+        this.telegramCinemaBot = telegramCinemaBot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot((LongPollingBot) telegramCinemaBot);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
--- src/test/java/pet/maikhun/telegramcinema/TelegramCinemaApplicationTests.java
+++ src/test/java/pet/maikhun/telegramcinema/TelegramCinemaApplicationTests.java
@@ -1,13 +1,8 @@
 package pet.maikhun.telegramcinema;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TelegramCinemaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
